aff1ee97004e1271b35f02ed7efbf1728aa8f71d
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 16:54:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, names|
-      names.each do |name|
-        if !final_array[name]
-          final_array = {}
-        end
-        if !final_array[name][key]
-          binding.pry
-          !final_array[name][key] = []
-        end
-        final_array[name][key].push[inner_key.to_s]
+  
       end
     end
   end
