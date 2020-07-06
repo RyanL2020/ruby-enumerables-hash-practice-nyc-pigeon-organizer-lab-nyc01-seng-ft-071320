2ead68bb50fbb9a9d467dd3d0c2982ae21ffb881
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 17:15:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,11 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
+      names.each do |name|
       binding.pry
+      end
+    end
     final_array
-end
+  end
 binding.pry
 end
-end 
